667e4131187e993beba7360242ae363a1f0f57cf
MACPV <aldo.pintado.98@gmail.com>
2025-06-13 01:06:11 -0500

    act

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var display = document.getElementById("resultado");
 
-function presion() {
+function presion(boton) {
+  pantalla(boton);
   return;
+}
+
+function pantalla(valor) {
+  let imprime;
+  imprime = borrar(valor == "C");
+  valor = borrar(valor == "C");
+  imprime = display.value + valor;
+  display.value = imprime;
+  console.log(imprime);
+}
+
+function borrar(valor) {
+  return "";
 }
