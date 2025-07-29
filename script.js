6601953a5298f0edb7f8c3ba99310ccc054c9d33
MACPV <aldo.pintado.98@gmail.com>
2025-07-28 22:16:36 -0500

    css futurista

--- script.js
+++ script.js
@@ -5,18 +5,87 @@ function presion(boton) {
   return;
 }
 
-function pantalla(valor) {
+let num1;
+let num2;
+let operador;
+let espera = false;
 
+function pantalla(valor) {
   if (valor == "C") {
     display.value = "";
     return;
   }
 
-  let imprime = display.value + valor;
-  display.value = imprime;
+  if (valor == "+") {
+    num1 = parseInt(display.value);
+    operador = "+";
+    display.value = "";
+    return;
+  }
+
+  if (valor == "-") {
+    num1 = parseInt(display.value);
+    operador = "-";
+    display.value = "";
+    return;
+  }
+
+  if (valor == "*") {
+    num1 = parseInt(display.value);
+    operador = "*";
+    display.value = "";
+    return;
+  }
+
+  if (valor == "/") {
+    num1 = parseInt(display.value);
+    operador = "/";
+    display.value = "";
+    return;
+  }
+
+  if (valor == "=") {
+    num2 = parseInt(display.value);
+    let resultado;
+    if (operador == "+") {
+      resultado = num1 + num2;
+    }
+    if (operador == "-") {
+      resultado = num1 - num2;
+    }
+    if (operador == "*") {
+      resultado = num1 * num2;
+    }
+    if (operador == "/") {
+      if (num2 == 0) {
+        resultado = "No puede ser cero";
+      } else {
+        resultado = num1 / num2;
+      }
+    }
+    display.value = resultado;
+    return;
+  }
+  display.value += valor;
 }
 
+function numActivo(espera) {}
 
+function setNum1(numero) {
+  num1 = numero;
+}
+
+function setNum2(numero) {
+  num2 = numero;
+}
+
+function getNum1() {
+  return num1;
+}
+
+function getNum2() {
+  return num2;
+}
 function borrar(valor) {
   return "";
 }
